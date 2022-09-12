764f211d344903a98384692081ef7f62ebaa43ed
苍之幻灵 <www.cpcgskill.com>
2022-09-13 06:52:13 +0800

    build(file:setup.py): 更新了打包脚本

--- setup.py
+++ setup.py
@@ -1,25 +1,55 @@
 #!/usr/bin/python
 # -*-coding:utf-8 -*-
 from __future__ import unicode_literals, print_function
+
+import sys
+
 import setuptools
 
+lib_name = "pyeal"
+
+author = "cpcgskill",
+author_email = "cpcgskill@outlook.com"
+
 version = '0.4.0'
+
+description = "Python 打包编译工具"
 with open("README.md", "rb") as f:
     long_description = f.read().decode(encoding="utf-8")
 
+project_homepage = "https://github.com/cpcgskill/pyeal"
+project_urls = {
+    "Bug Tracker": "https://github.com/cpcgskill/seal/issues",
+}
+license = "Apache Software License (Apache 2.0)"
+
+python_requires = ">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, !=3.5.*"
+install_requires = [
+    'astunparse==1.6.3',
+]
+
+if sys.version_info.major < 3:
+    console_scripts = [
+        "pyeal=pyeal.cli:main",
+        "pyeal-py2=pyeal.cli:main",
+    ]
+else:
+    console_scripts = [
+        "pyeal=pyeal.cli:main",
+        "pyeal-py3=pyeal.cli:main",
+    ]
+
 setuptools.setup(
-    name="pyeal",
+    name=lib_name,
     version=version,
-    author="cpcgskill",
-    author_email="cpcgskill@outlook.com",
-    description="Python 打包编译工具",
+    author=author,
+    author_email=author_email,
+    description=description,
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/cpcgskill/pyeal",
-    project_urls={
-        "Bug Tracker": "https://github.com/cpcgskill/seal/issues",
-    },
-    license="Apache Software License (Apache 2.0)",
+    url=project_homepage,
+    project_urls=project_urls,
+    license=license,
     classifiers=[
         'License :: OSI Approved :: Apache Software License',
         'Programming Language :: Python',
@@ -37,18 +67,13 @@ setuptools.setup(
     # # 使用自动搜索
     # packages=setuptools.find_packages(where="src"),
     packages=['pyeal', 'pyeal.code'],
-    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, !=3.5.*",
+    python_requires=python_requires,
     # 指定依赖
-    install_requires=[
-        'astunparse==1.6.3',
-    ],
+    install_requires=install_requires,
     # 指定启用包数据如log.ico这样的文件
     include_package_data=True,
-
     entry_points={
         # 控制台脚本
-        "console_scripts": [
-            "pyeal=pyeal.cli:main",
-        ],
+        "console_scripts": console_scripts,
     },
 )
