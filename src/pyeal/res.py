9afe9e32735e760d05dad033a382735ce16a3d90
苍之幻灵 <www.cpcgskill.com>
2022-09-13 13:37:29 +0800

    feat(module: pyeal.res): 大幅度更新了seal模块，优化实现，提供了更完整的功能

--- src/pyeal/res.py
+++ src/pyeal/res.py
@@ -16,98 +16,257 @@ from abc import abstractmethod
 
 from pyeal.exc import *
 
+if False:
+    from typing import List, Tuple, Dict, AnyStr, Any
+
 
 class BaseRes(object):
     @abstractmethod
-    def read(self, p):
+    def customize_read(self, path):
         """
-        :type p: unicode
+        :type path: AnyStr
         :rtype: bytes
         """
 
     @abstractmethod
-    def write(self, p, data):
+    def customize_write(self, path, data):
         """
-        :type p: unicode
+        :type path: AnyStr
         :type data: bytes
-        :return:
-        """
-
-    def read_string(self, p):
-        return self.read(p).decode('utf-8')
-
-    def write_string(self, p, s):
-        self.write(p, s.encode('utf-8'))
-
-    @abstractmethod
-    def clean(self, p=None):
-        """
-        :rtype:
         """
 
     @abstractmethod
-    def sep(self):
+    def customize_remove(self, path):
         """
-        :rtype: unicode
+        :type path: AnyStr
         """
 
     @abstractmethod
-    def walk(self):
+    def customize_make_dir(self, path):
+        """
+        :type path: AnyStr
         """
 
-        :rtype: list[tuple[unicode|None, list[unicode]]]
+    @abstractmethod
+    def customize_remove_dir(self, path):
+        """
+        :type path: AnyStr
         """
 
-    def files(self):
-        for r, fs in self.walk():
-            for f in fs:
-                if r is not None:
-                    f = self.sep().join((r, f))
+    @abstractmethod
+    def customize_sep(self):
+        """
+        :rtype: AnyStr
+        """
+
+    @abstractmethod
+    def customize_is_dir(self, path):
+        """
+        :type path: AnyStr
+        :rtype: bool
+        """
+
+    @abstractmethod
+    def customize_is_file(self, path):
+        """
+        :type path: AnyStr
+        :rtype: bool
+        """
+
+    @abstractmethod
+    def customize_walk(self):
+        """
+
+        :rtype: List[Tuple[AnyStr, List[AnyStr], List[AnyStr]]]
+        """
+
+    def to_customize_sep(self, path):
+        """
+        :type path: AnyStr
+        :rtype: AnyStr
+        """
+        return self.customize_sep().join((i for i in path.split('/') if i != ''))
+
+    def to_std_sep(self, path):
+        """
+        :type path: AnyStr
+        :rtype: AnyStr
+        """
+        return '/'.join((i for i in path.split(self.customize_sep()) if i != ''))
+
+    @staticmethod
+    def join_path(*path):
+        return '/'.join(path)
+
+    def read(self, path):
+        """
+        :type path: AnyStr
+        :rtype: bytes
+        """
+        return self.customize_read(self.to_customize_sep(path))
+
+    def write(self, path, data):
+        """
+        :type path: AnyStr
+        :type data: bytes
+        """
+        self.customize_write(self.to_customize_sep(path), data)
+        return self
+
+    def read_string(self, path):
+        """
+        :type path: AnyStr
+        :rtype: AnyStr
+        """
+        return self.read(path).decode('utf-8')
+
+    def write_string(self, path, data):
+        """
+        :type path: AnyStr
+        :type data: AnyStr
+        """
+        self.write(path, data.encode('utf-8'))
+        return self
+
+    def remove(self, path):
+        self.customize_remove(self.to_customize_sep(path))
+        return self
+
+    def make_dir(self, path):
+        path = self.to_customize_sep(path)
+        if not self.customize_is_dir(path):
+            self.customize_make_dir(path)
+        return self
+
+    def remove_dir(self, path):
+        path = self.to_customize_sep(path)
+        if self.customize_is_dir(path):
+            self.customize_remove_dir(path)
+        return self
+
+    def is_dir(self, path):
+        path = self.to_customize_sep(path)
+        return self.customize_is_dir(path)
+
+    def is_file(self, path):
+        path = self.to_customize_sep(path)
+        return self.customize_is_file(path)
+
+    def walk(self, prefix=''):
+        """
+        :type prefix: AnyStr
+        :rtype: List[Tuple[AnyStr, List[AnyStr], List[AnyStr]]]
+        """
+        prefix = self.to_std_sep(prefix)
+        data = [(self.to_std_sep(root), dirs, files) for root, dirs, files in self.customize_walk()]
+        # 如果前缀不为空，则过滤所有不和前缀匹配的root
+        if prefix != '':
+            prefix = prefix.split('/')
+
+            def path_prefix_match(matched_path):
+                matched_path = matched_path.split('/')
+                if len(prefix) > len(matched_path):
+                    return True
+                matched_path = matched_path[:len(prefix)]
+                return all([i[0] == i[1] for i in zip(prefix, matched_path)])
+
+            return [(root, dirs, files) for root, dirs, files in data if path_prefix_match(root)]
+        return data
+
+    def __files(self, prefix=''):
+        for root, dirs, files in self.walk(prefix):
+            for f in files:
+                if root != '':
+                    f = '/'.join((root, f))
                 yield f
+
+    def files(self, prefix=''):
+        """
+        :type prefix: AnyStr
+        :rtype: List[AnyStr]
+        """
+        return list(self.__files(prefix))
+
+    def __dirs(self, prefix=''):
+        for root, dirs, files in self.walk(prefix):
+            for d in dirs:
+                if root != '':
+                    d = '/'.join((root, d))
+                yield d
+
+    def dirs(self, prefix=''):
+        """
+        :type prefix: AnyStr
+        :rtype: List[AnyStr]
+        """
+        return list(self.__dirs(prefix))
+
+    def clean(self, path=''):
+        """
+        :type path: AnyStr
+        """
+        for f in self.files(path):
+            self.customize_remove(f)
+        for d in self.dirs(path):
+            self.customize_remove_dir(d)
+        return self
 
 
 class LocalRes(BaseRes):
     def __init__(self, root):
         """
 
-        :type root: unicode
+        :type root: AnyStr
         """
-        self.root = root
+        self.root = os.path.abspath(root)
 
-    def read(self, p):
-        p = self.sep().join((self.root, p))
-        with open(p, "rb") as f:
+    def __str__(self):
+        return str(self.__unicode__())
+
+    def __unicode__(self):
+        return "{}(root={})".format(self.__class__.__name__, repr(self.root))
+
+    def customize_read(self, path):
+        path = os.sep.join((self.root, path))
+        with open(path, "rb") as f:
             return f.read()
 
-    def write(self, p, data):
-        p = self.sep().join((self.root, p))
-        dir_, name = os.path.split(p)
+    def customize_write(self, path, data):
+        path = os.sep.join((self.root, path))
+        dir_, name = os.path.split(path)
         if not os.path.isdir(dir_):
             os.makedirs(dir_)
-        with open(p, "wb") as f:
+        with open(path, "wb") as f:
             return f.write(data)
 
-    def clean(self, p=None):
-        file_list = []
-        dir_list = []
-        root = self.root if p is None else self.sep().join([self.root, p])
-        for root, ds, fs in os.walk(root):
-            for i in fs:
-                file_list.append(self.sep().join([root, i]))
-            for i in ds:
-                dir_list.append(self.sep().join([root, i]))
-        for i in file_list:
-            os.remove(i)
-        for i in reversed(dir_list):
-            os.rmdir(i)
+    def customize_remove(self, path):
+        path = os.sep.join((self.root, path))
+        os.remove(path)
 
-    def sep(self):
+    def customize_make_dir(self, path):
+        path = os.sep.join((self.root, path))
+        os.mkdir(path)
+
+    def customize_remove_dir(self, path):
+        path = os.sep.join((self.root, path))
+        os.removedirs(path)
+
+    def customize_sep(self):
         return os.sep
 
-    def walk(self):
-        for root, _, files in os.walk(self.root):
-            root = self.sep().join(root.split(self.sep())[len(self.root.split(self.sep())):])
-            yield None if root == "" else root, files
+    def customize_is_dir(self, path):
+        path = os.sep.join((self.root, path))
+        return os.path.isdir(path)
+
+    def customize_is_file(self, path):
+        path = os.sep.join((self.root, path))
+        return os.path.isfile(path)
+
+    def customize_walk(self):
+        for root, dirs, files in os.walk(self.root):
+            root = root.split(self.customize_sep())[len(self.root.split(self.customize_sep())):]
+            root = self.customize_sep().join(root)
+            yield root, dirs, files
 
 
 class DirectoryRes(BaseRes):
@@ -115,34 +274,53 @@ class DirectoryRes(BaseRes):
         """
 
         :type res: BaseRes
-        :type dir_: unicode
+        :type dir_: AnyStr
         """
         self.res = res
-        self.dir = dir_
+        self.dir = self.to_std_sep(dir_)
+        self.dir_path_len = len(self.dir.split('/'))
 
-    def new_path(self, p):
-        return self.sep().join((self.dir, p))
+    def __str__(self):
+        return str(self.__unicode__())
 
-    def read(self, p):
-        return self.res.read(self.new_path(p))
+    def __unicode__(self):
+        return "{}(res={}, dir={})".format(self.__class__.__name__, self.res, repr(self.dir))
 
-    def write(self, p, data):
-        return self.res.write(self.new_path(p), data)
+    def new_customize_path(self, p):
+        return self.customize_sep().join((self.dir, p))
 
-    def sep(self):
-        return self.res.sep()
+    def new_std_path(self, p):
+        return '/'.join((self.dir, p))
 
-    def clean(self, p=None):
-        p = self.dir if p is None else self.new_path(p)
-        self.res.clean(p)
+    def customize_read(self, path):
+        return self.res.read(self.new_customize_path(path))
 
-    def walk(self):
-        for r, fs in self.res.walk():
-            if r is not None and self.dir in r:
-                rs = r.split(self.sep())
-                if self.dir == rs[0]:
-                    r = self.sep().join(rs[1:])
-                    yield None if r == "" else r, fs
+    def customize_write(self, path, data):
+        return self.res.write(self.new_customize_path(path), data)
+
+    def customize_remove(self, path):
+        return self.res.customize_remove(self.new_customize_path(path))
+
+    def customize_make_dir(self, path):
+        return self.res.customize_make_dir(self.new_customize_path(path))
+
+    def customize_remove_dir(self, path):
+        return self.res.customize_remove_dir(self.new_customize_path(path))
+
+    def customize_sep(self):
+        return self.res.customize_sep()
+
+    def customize_is_dir(self, path):
+        return self.res.customize_is_dir(self.new_customize_path(path))
+
+    def customize_is_file(self, path):
+        return self.res.customize_is_file(self.new_customize_path(path))
+
+    def customize_walk(self):
+        return [
+            ('/'.join(root.split('/')[self.dir_path_len:]), dirs, files)
+            for root, dirs, files in self.res.walk(self.dir)
+        ]
 
 
 class MergeRes(BaseRes):
@@ -153,30 +331,36 @@ class MergeRes(BaseRes):
         """
         self.res = res
 
-    def read(self, p):
+    def customize_read(self, path):
         for r in self.res:
             try:
-                return r.read(p)
+                return r.read(path)
             except:
                 pass
         raise ResException("找不到对应文件")
 
-    def write(self, p, data):
-        return self.res[0].write(p, data)
+    def customize_write(self, path, data):
+        raise RuntimeError('写入相关方法不可用')
 
-    def sep(self):
-        return self.res[0].sep()
+    def customize_remove(self, path):
+        raise RuntimeError('写入相关方法不可用')
 
-    def clean(self, p=None):
-        for r in self.res:
-            r.clean(p)
+    def customize_make_dir(self, path):
+        raise RuntimeError('写入相关方法不可用')
 
-    def walk(self):
-        for r in self.res:
-            for r, fs in r.walk():
-                yield r, fs
+    def customize_remove_dir(self, path):
+        raise RuntimeError('写入相关方法不可用')
 
+    def customize_sep(self):
+        return '/'
 
-if __name__ == "__main__":
-    for i in LocalRes(r"D:\backup_to_cloud\dev\python_for_maya\package\seal").walk():
-        print(i)
+    def customize_is_dir(self, path):
+        return any((i.is_dir(i.to_customize_sep(path)) for i in self.res))
+
+    def customize_is_file(self, path):
+        return any((i.is_file(i.to_customize_sep(path)) for i in self.res))
+
+    def customize_walk(self):
+        for i in zip(*(i.walk() for i in self.res)):
+            for t in i:
+                yield t
